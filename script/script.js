24a28bf6788b26fd111a84a14bc6f7b91843f74c
Vladislav Kovalev <max2433186@gmail.com>
2020-03-27 11:12:51 +0300

    Finished lesson02

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
 let money, income, addExpenses, deposit, mission, period;
-alert('Alert');
-console.log('Console');
+
+money = 1200;
+income = 'Форекс';
+addExpenses = 'интернет, телевидение, коммуналка';
+deposit = true;
+mission = 1234567;
+period = 7;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+
+console.log('Период равен %d месяцев',period);
+console.log('Цель заработать %d рублей',mission);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money/30;
+console.log(budgetDay);
