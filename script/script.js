8e9b81aa9b7200fb27c43ac37a99fb6f1bbda85a
Vladislav Kovalev <max2433186@gmail.com>
2020-04-01 11:39:25 +0300

    Finished hard lesson04

--- script/script.js
+++ script/script.js
@@ -1,34 +1,13 @@
 //1
 'use strict';
-let lang  = prompt("Write language('ru' or 'en'");
-let daysOnRu = ['Понедельник','Вторник','Среда','Четверг','Пятница','Субота','Воскресенье'];
-let daysOnEn = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
 
-//if
-if(lang === 'ru') console.log(daysOnRu);
-else if(lang === 'en') console.log(daysOnEn);
+const stringFormatting = (str) => {
 
-//switch
-switch(lang){
-    case 'ru':
-        console.log(daysOnRu);
-        break;
-    case 'en':
-        console.log(daysOnEn);
-        break;
-}
-
-//array
-
-let daysOnRuAndEn =new Map([
-    ["ru",daysOnRu],
-    ["en",daysOnEn]
-]);
-console.log(daysOnRuAndEn.get(lang));
-
-//2
-let namePerson = prompt("Write your name");
-
-let resultForNamePerson = namePerson === "Артём" ? "директор" : namePerson === "Максим" ? "преподаватель" : "студент";
-
-console.log(resultForNamePerson);
+    if(typeof(str) !== "string") {
+        console.warn("Передана не строка");
+        return null;
+    }
+    let result = str.trim();
+    result = result.length>30 ? result.substring(0,30)+"..." : result;
+    return result;
+};
