8a41ee234be51931970c963156a78b0e68af5255
Vladislav Kovalev <max2433186@gmail.com>
2020-04-01 17:12:22 +0300

    Finished hard lesson05

--- script/script.js
+++ script/script.js
@@ -1,13 +1,32 @@
 //1
 'use strict';
 
-const stringFormatting = (str) => {
+let arr = [];
 
-    if(typeof(str) !== "string") {
-        console.warn("Передана не строка");
-        return null;
+for(let i = 0; i<7; i++){
+    arr[i] = Math.floor(Math.random()*10)+"234567";
+}
+
+for(let i = 0; i<arr.length; i++){
+    if(arr[i].charAt(0) === "2" || arr[i].charAt(0) === "4"){
+        console.log(arr[i]);
     }
-    let result = str.trim();
-    result = result.length>30 ? result.substring(0,30)+"..." : result;
-    return result;
-};
+}
+
+for(let i = 2; i<=100; i++){
+
+    let isPrimeNumber = true;
+    for(let j = 2; j<8; j++){
+        if(i%j===0) {
+            isPrimeNumber = false;
+            break;
+        }
+    }
+
+    if(isPrimeNumber || 
+        i === 2 || 
+        i === 3 ||
+        i === 5 ||
+        i === 7) console.log(i+" Делители этого числа: 1 и "+i);
+
+}
