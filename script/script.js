326aeaf9618d0db748f9883ae1a1fba067464e22
Vladislav Kovalev <max2433186@gmail.com>
2020-04-03 14:33:02 +0300

    Finished hard lesson07

--- script/script.js
+++ script/script.js
@@ -1,73 +1,19 @@
 'use strict';
 
-const game = function() {
+let week = ['Понедельник','Вторник','Среда','Четверг','Пятница','Субота','Воскресенье'];
+let today = new Date();
 
-    const guessedNumber = Math.floor(Math.random()*100);
-    let number = prompt("Угадайте число от 1 до 100");
-    let attempts = 10;
+let div = document.getElementsByClassName('week');
 
-    //конец игры
-    const theEnd = () => { alert("Надеюсь вы получили удовольствие от игры :)"); };
-
-    // число ли?
-    const isNum = function(n) {
-        return !isNaN(parseFloat(n)) && isFinite(n);
-    };
-
-
-
-    const start = function f () {
-
-        //проверки
-        if(number === null){
-            theEnd();
-            return;
-        } else if(!isNum(number)){
-            number = prompt("Введи число!");
-            start(attempts);
-            return;
-        } else{
-            number = +number;
-        }
-
-
-        attempts--;
-        if(attempts===0){
-            if(confirm("Попытки закончились, хотите сыграть еще?")) {
-                game();
-                return;
-            } else{
-            theEnd();
-            return;
-            }
-        }
-        
-        
-        //действия
-        if(number<guessedNumber||number>guessedNumber){
-
-            let moreOrLess = number<guessedNumber ? "Загаданное число больше" :
-            "Загаданное число меньше";
-
-            number = prompt(moreOrLess+", осталось попыток "+attempts);
-            start();
-            
-        } else {
-
-            if(confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")){
-                game();
-            }else{
-                theEnd();
-            }
-            
-        }
-
-    };
-
-
-
-
-    return start();
-};
-
-game();
+for(let i = 0; i<week.length; i++){
+    let day = document.createElement('p');
+    day.style.fontSize = "32px";
+    day.innerHTML = week[i];
+    if(i===5||i===6){
+        day.innerHTML = "<em>"+week[i]+"</em>";
+    }
+    if(today.getDay()===i+1||(today.getDay()===0&&i===6)){
+        day.innerHTML="<strong>"+week[i]+"</strong";
+    }
+    document.body.append(day);
+}
