c063372da259aa90a6a61e152e15f8768993e7b8
Vladislav Kovalev <max2433186@gmail.com>
2020-04-24 15:08:27 +0300

    Finished practic lesson18

--- script/script.js
+++ script/script.js
@@ -1,6 +1,40 @@
+/* eslint-disable indent */
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
 'use strict';
-const city = ['москва', 'ekat', 'gently', 'exhausting'];
+window.addEventListener('DOMContentLoaded', () => {
+	const countTimer = deadline => {
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
 
-const fixCity = city.map(city => city[0].toUpperCase + city.slice(1));
 
-console.log(fixCity);
+
+        const getTimeRemaining = () => {
+            let dateStop = new Date(deadline),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor(timeRemaining / 60 / 60) % 24;
+            return { timeRemaining, hours, minutes, seconds };
+        };
+
+        const updateClock = () => {
+            const timer = getTimeRemaining();
+
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+
+            if (timer.timeRemaining > 0) {
+                setTimeout(updateClock, 1000);
+            }
+        };
+
+
+        updateClock();
+    };
+
+    countTimer('25 april 2020');
+});
