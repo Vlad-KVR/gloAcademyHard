86f14e0f0517b7a66e126c1ab23cfae49b6f1917
Vladislav Kovalev <max2433186@gmail.com>
2020-04-30 12:15:52 +0300

    Finished main task lesson25

--- script/script.js
+++ script/script.js
@@ -401,6 +401,28 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	calculator(100);
 
+	//validation-form
+	const validationForm = () => {
+		const formPhones = document.querySelectorAll('.form-phone'),
+			formName = document.querySelectorAll('.form-name'),
+			mess = document.querySelector('.mess');
+
+
+		formPhones.forEach(item => {
+			item.addEventListener('input', () => { 
+				item.value = item.value.replace(/[^0-9+]/g, ''); 
+			});
+		});
+		[...formName, mess].forEach(item => {
+			item.addEventListener('input', () => {
+				item.value = item.value.replace(/[^А-Яа-я ]/g, '');
+			});
+		});
+	};
+
+	validationForm();
+
+
 	//send-ajax-form
 
 	const sendForm = () => {
